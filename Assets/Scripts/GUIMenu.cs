884cb9404e6ab7226abc18c694e826e038db04c7
zeccus <zeccus@gmail.com>
2014-12-08 00:24:26 +0100

    Escena menu

---
+++ Assets/Scripts/GUIMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIMenu : MonoBehaviour {
+
+	void OnMouseDown(){
+		if (this.gameObject.name == "Start") {
+			Application.LoadLevel("Escena1");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
