85aeda6f7b097c2cd531ef96f17c43c8eead6ba5
zeccus <zeccus@gmail.com>
2014-12-08 03:44:36 +0100

    script portales y trampolines

--- Assets/Scripts/Trampolin.cs
+++ Assets/Scripts/Trampolin.cs
@@ -4,9 +4,17 @@ using System.Collections;
 public class Trampolin : MonoBehaviour {
 	public Vector2 fuerza;
 	public bool izquierda;
+	public SpriteRenderer sr;
+	public Sprite cargado;
+	public Sprite liberado;
 	// Use this for initialization
 	void Start () {
+		sr = this.GetComponent ("SpriteRenderer") as SpriteRenderer;
+	}
 
+	IEnumerator cargar(){
+		yield return new WaitForSeconds (0.2f);
+		sr.sprite = cargado;
 	}
 
 	void OnTriggerEnter2D(Collider2D col){
@@ -14,10 +22,14 @@ public class Trampolin : MonoBehaviour {
 			if(izquierda){
 				col.gameObject.rigidbody2D.velocity=Vector2.zero;
 				col.gameObject.rigidbody2D.AddForce(new Vector2(fuerza.x*-1, fuerza.y), ForceMode2D.Impulse);
+				sr.sprite=liberado;
+				StartCoroutine("cargar");
 			}
 			else{
 				col.gameObject.rigidbody2D.velocity=Vector2.zero;
 				col.gameObject.rigidbody2D.AddForce(fuerza,ForceMode2D.Impulse);
+				sr.sprite=liberado;
+				StartCoroutine("cargar");
 			}
 		}
 	}
