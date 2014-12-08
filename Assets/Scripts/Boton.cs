1fcaa29dba40d927e434cff31c2b8b8f4987a6bc
rhastapunk <rhastapunk@gmail.com>
2014-12-08 13:26:59 +0100

    10 escenas terminadas

--- Assets/Scripts/Boton.cs
+++ Assets/Scripts/Boton.cs
@@ -5,12 +5,12 @@ public class Boton : MonoBehaviour {
 
 	public bool p;
 	public RaycastHit2D hit;
-	public ControladorPersonaje scrptCP;
-	public GameObject player; 
+
+	public bool isUp;
 	// Use this for initialization
 	void Start () {
 		p = false;
-		scrptCP = player.GetComponent ("ControladorPersonaje") as ControladorPersonaje;
+
 	}
 
 
@@ -18,7 +18,7 @@ public class Boton : MonoBehaviour {
 	void Update () {
 		//Detectamos que los rayos estan en contacto con el personaje (ya sea copia u original)
 		// para marcar el boton como pulsado
-		if (scrptCP.girado==false) {
+		if (!isUp) {
 						Debug.DrawLine (new Vector2 (this.transform.position.x, this.transform.position.y), new Vector2 (this.transform.position.x, this.transform.position.y + 0.2f));
 						p = Physics2D.Linecast (new Vector2 (this.transform.position.x, this.transform.position.y), new Vector2 (this.transform.position.x, this.transform.position.y + 0.2f), 1 << 9); 
 		} else {
---
+++ Assets/Scripts/MovimientoObjeto.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimientoObjeto : MonoBehaviour {
+	public float minX, maxX, iniX, iniY;
+	public bool right,initRight;
+	public float moveSpeed;
+	Quaternion q;
+	public GameObject player;
+
+	public ControladorPersonaje scrptPJ;
+	public ViajesTemporales scrptVT;
+	// Use this for initialization
+	void Start () {
+		initRight = right;
+		q = new Quaternion ();
+		iniX = this.transform.position.x;
+		iniY = this.transform.position.y;
+		if (right) {
+			q = Quaternion.Euler(0f,180f,0f);
+			transform.rotation = q;
+		}
+		scrptPJ = player.GetComponent ("ControladorPersonaje") as ControladorPersonaje;
+		scrptVT = Camera.main.GetComponent ("ViajesTemporales") as ViajesTemporales;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+		if(Input.GetKeyDown(KeyCode.Space)&&(scrptPJ.isGrounded)){
+			if(scrptVT.nTravels >= 0){
+				this.transform.position=new Vector3(iniX, iniY,0f);
+				right=initRight;
+			}
+			
+		}
+		
+	}
+
+	public void Move(){
+		if (right) {
+			this.transform.position += new Vector3(moveSpeed, 0f,0f);
+			if (maxX <= this.transform.position.x) {
+				q = Quaternion.Euler(0f,0f,0f);
+				transform.rotation = q;
+				right = false;
+			}
+		} else {
+			this.transform.position -= new Vector3(moveSpeed, 0f,0f);
+			if (minX >= this.transform.position.x) {
+				q = Quaternion.Euler(0f,180f,0f);
+				transform.rotation = q;
+				right = true;
+			}
+		}
+	}
+	}
+
